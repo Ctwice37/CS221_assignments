6dcf6369cd7ae8f7b24e18d7fb6ce6496b382e42
Charles Carpenter <carpe359@gmail.com>
2022-01-24 14:16:33 -0600

    assignment_6

---
+++ src/assignment_7/employee.java
@@ -0,0 +1,17 @@
+package assignment_7;
+
+public class employee {
+
+    private String name;
+
+        public employee(String name){
+            this.name = name;
+        }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
+
+
